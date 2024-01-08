a836c67eeb6e3439a1eb886a3c55b7a292377006
sgonzales-vidarxpert <sebastian.gonzalez@vidarxpert.com>
2024-01-08 19:43:37 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FaDiscord, FaTelegram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import logo from './memewolf.png'
+import logo from "./memewolf.png";
 
 const gifArray = [
   "https://media.tenor.com/F2k3cmN7-RUAAAAM/dancing-wolf.gif",
@@ -21,7 +21,7 @@ const gifArray = [
   "https://24.media.tumblr.com/de1e260e2d23500937fd099ae91a2903/tumblr_mrj469B3cZ1sf51yqo1_500.gif",
   "https://i.makeagif.com/media/8-15-2015/01bRqA.gif",
   "https://64.media.tumblr.com/bece87e0bcaab29d0bc832be7011c3b6/b45d6b66b8e66790-66/s540x810/de132be1a251e4d3fc510d1ef0646485576c3348.gifv",
-'http://localhost:3000/static/media/memewolf.c1c745cd59096faea3bd.png'
+  "http://localhost:3000/static/media/memewolf.c1c745cd59096faea3bd.png",
   // Add more URLs as needed
 ];
 
@@ -46,6 +46,7 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(true);
   const [gifs, setGifs] = useState([]);
   const [playerVisible, setPlayerVisible] = useState(true);
+  const [modalShow, setModalShow] = useState(false);
 
   useEffect(() => {
     // Display a new GIF every second
@@ -82,47 +83,95 @@ function App() {
     <div className="App  ">
       <div className="  items-center top-10 w-[96vw] absolute  left-[2vw] z-[1000]  bg-white p-4 rounded-lg shadow-md ">
         <div className="flex items-center justify-between">
-          <img src={logo} className="w-20 absolute" alt='logo'/>
+          <img src={logo} className="w-20 absolute" alt="logo" />
           <p className="font-bold p-2   ">$dolf</p>
-          <p className="font-bold p-2 text-[0.6rem]  ">Welcome to $DOLF = Dancing Wolf </p>
+          <p className="font-bold p-2 text-[0.6rem]  ">
+            Welcome to $DOLF (Dancing Wolf){" "}
+          </p>
           <div className="flex gap-4 items-center">
-            <FaDiscord className="text-[1.5rem] text-purple-500" />
-            <FaTelegram className="text-[1.5rem] text-sky-400" />
-            <FaXTwitter className="text-[1.5rem]" />
+            <a href="https://www.discord.gg/KxKAyntQFZ">
+              <FaDiscord className="text-[1.5rem] text-purple-500" />{" "}
+            </a>
+            <a href="https://www.discord.gg/KxKAyntQFZ">
+              {" "}
+              <FaTelegram className="text-[1.5rem] text-sky-400" />
+            </a>
+            <a href="https://twitter.com/DyorWolfz">
+              {" "}
+              <FaXTwitter className="text-[1.5rem]" />{" "}
+            </a>
           </div>
         </div>
       </div>
-      <div className="p-4 top-[46vh] shadow-lg left-[44vw] z-[100] absolute  cursor-pointer rounded-lg bg-gradient-to-r from-orange-500 to-yellow-500 ">Buy Pre-sale</div>
-      <div className=" items-center  absolute bottom-10 w-[96vw] z-[1000]  left-[2vw]  bg-white p-4 rounded-lg shadow-md border-2 ">
-        <div className="flex justify-between">
-          <div className="flex gap-4">
-            <button className="font-bold  text-slate-600 hover:bg-orange-400 p-2 rounded-lg hover:text-white transition-all 2s">
-              Tokenomics
-            </button>
-            <button className="font-bold text-slate-600 hover:bg-orange-400 p-2 rounded-lg hover:text-white transition-all 2s">
-              Chart
-            </button>
-            <button className="font-bold text-slate-600 hover:bg-orange-400 p-2 rounded-lg hover:text-white transition-all 2s">
-              Buy now
-            </button>
+      <a
+        href="https://app.hel.io/pay/659c73ea199fbe7c7c9c7020"
+        className=" top-[46vh] hover:text-white p-4  left-[44vw] z-[100] absolute  cursor-pointer rounded-lg bg-gradient-to-r from-orange-500 to-yellow-500 "
+      >
+        Buy Pre-sale
+      </a>
+      <div className=" items-center  absolute bottom-10 w-[96vw] z-[1000]  left-[2vw]   p-4 rounded-lg shadow-md ">
+        {modalShow && (
+          <div className="bg-white h-[200px] rounded-lg shadow-md w-[200px] transition-all 2s ease-in mb-4 relative items-center justify-center flex flex-col gap-4">
+            <p>Supply: 1b</p>
+            <p>Airdrops: 20%</p>
+            <p>Presale: 30% </p>
+            <p>Marketing: 10% </p>
+            <p>Liquidity Pool (LP): 30% </p>
           </div>
-          <div className="flex items-center mr-40 text-gray-700">
-            CA: WULjeiZZXiBb8fBvSjmEqoeBX57NFEAGmMdftqPPtJj
-          </div>
-          <div className="flex gap-4">
-            <button onClick={() => goBackward()}>
-              <FontAwesomeIcon className="text-[1.2rem]" icon={faStepBackward} />
-            </button>
-            <button onClick={() => toggleMusic()}>
-              {isPlaying ? (
-                <FontAwesomeIcon className="text-[1.2rem]" icon={faPause} />
-              ) : (
-                <FontAwesomeIcon className="text-[1.2rem]" icon={faPlay} />
-              )}
-            </button>
-            <button onClick={() => goForward()}>
-              <FontAwesomeIcon className="text-[1.2rem]" icon={faStepForward} />
-            </button>
+        )}
+        <div className="bg-white p-2 shadow-lg rounded-lg">
+          <div className="flex justify-between">
+            <div className="flex gap-4">
+              <button
+                onClick={() => setModalShow(!modalShow)}
+                className="font-bold  text-slate-600 hover:bg-orange-400 p-2 rounded-lg hover:text-white transition-all 2s"
+              >
+                Tokenomics
+              </button>
+              <button className="font-bold text-slate-600 hover:bg-orange-400 p-2 rounded-lg hover:text-white transition-all 2s">
+                Chart
+              </button>
+              <button className="font-bold text-slate-600 hover:bg-orange-400 p-2 rounded-lg hover:text-white transition-all 2s">
+                Buy now
+              </button>
+              <a
+                href="https://www.tensor.trade/trade/dyor_wolfz"
+                className="font-bold text-white bg-slate-800 p-2 rounded-lg hover:text-white transition-all 2s"
+              >
+                Buy on Tensor
+              </a>
+              <a
+                href="https://www.tensor.trade/trade/dyor_wolfz"
+                className="font-bold text-white bg-pink-400 p-2 rounded-lg hover:text-white transition-all 2s"
+              >
+                Buy on ME
+              </a>
+            </div>
+
+            <div className="flex gap-4">
+              <div className="flex items-center mr-10 text-gray-700">
+                CA: not created yet
+              </div>
+              <button onClick={() => goBackward()}>
+                <FontAwesomeIcon
+                  className="text-[1.2rem]"
+                  icon={faStepBackward}
+                />
+              </button>
+              <button onClick={() => toggleMusic()}>
+                {isPlaying ? (
+                  <FontAwesomeIcon className="text-[1.2rem]" icon={faPause} />
+                ) : (
+                  <FontAwesomeIcon className="text-[1.2rem]" icon={faPlay} />
+                )}
+              </button>
+              <button onClick={() => goForward()}>
+                <FontAwesomeIcon
+                  className="text-[1.2rem]"
+                  icon={faStepForward}
+                />
+              </button>
+            </div>
           </div>
         </div>
       </div>
